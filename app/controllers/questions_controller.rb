41ac4dc10148d0b7198bd16b49f652479c28972c
Luke Johnson <lukejohnson1@outlook.com>
2024-10-10 18:34:57 +1100

    dumb coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,13 @@ class QuestionsController < ApplicationController
   def ask
   end
   def answer
-    @answers
+    case
+      when params["answer"] == "I am going to work"
+        @answer = "Great!"
+      when params["answer"].end_with?("?")
+        @answer = "Silly question, get dressed and go to work!"
+      else
+        @answer = "I don't care, get dressed and go to work"
+    end
   end
 end
